d413ce09405bddb30a6f2a50e2c72cdf9fb7ca69
Rococoro <73629584+Rococoro@users.noreply.github.com>
2025-06-07 17:43:09 +0900

    Update gather_kernel.cu

--- src/gather_kernel.cu
+++ src/gather_kernel.cu
@@ -1,13 +1,20 @@
 #include "gather_kernel.cuh"
 
-/* 256‑thread 블록 기준: 1 스레드 - 1 변경 블록 */
+//1 스레드 - 1 변경된 블록
 __global__
 void gather_ptrs(const int32_t* __restrict__ deltaIdx, const char* __restrict__ base, std::size_t blkSz, char** __restrict__ outPtrs, std::size_t M)
 {
     std::size_t k = blockIdx.x * blockDim.x + threadIdx.x;
-    if (k >= M) return;
+    extern __shared__ char* s_ptr[];
+    // 주소 계산
+    char* addr = nullptr; //if문 밖의 thread는 null
+    if (k < M) {
+        int32_t blkID = deltaIdx[k];
+        addr = const_cast<char*>(base) + static_cast<std::size_t>(blkID) * blkSz;
+    }
+    s_ptr[threadIdx.x] = addr;
 
-    int32_t blkID = deltaIdx[k];               // 변경 블록 번호
-    outPtrs[k]    = const_cast<char*>(base)    // 원본 버퍼 시작 +
-                  + static_cast<std::size_t>(blkID) * blkSz;
+    __syncthreads(); // 모든 주소 계산 완료에 대한 coalescing
+    if (k < M) // 범위 안 스레드만 실제 결과 저장
+        outPtrs[k] = s_ptr[threadIdx.x];
 }
