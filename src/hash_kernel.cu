91262245a21b1e3287cdeec93062bc0da12620d1
Rococoro <73629584+Rococoro@users.noreply.github.com>
2025-06-07 17:43:22 +0900

    Update hash_kernel.cu

--- src/hash_kernel.cu
+++ src/hash_kernel.cu
@@ -1,17 +1,20 @@
+#include "hash_kernel.cuh"
 
-#include "MurmurHash2.cuh"
 
 __device__ __forceinline__
-uint64_t MurmurHash64A_GPU(const void* key, int len, uint64_t seed = 0x1e35a7bdUL)
+uint64_t MurmurHash64A_GPU(const void* key, int len, uint64_t seed = 0x1e35a7bdULL)
 {
     return MurmurHash64A(key, len, seed);
 }
 
 //d_in: 원본 바이트 버퍼, d_out: 해쉬 결과 배열(device)
 //blk_sz, N: 블락 사이즈, 개수
-__global__ void hash_kernel(const char* d_in, uint64_t* d_out, size_t blk_sz, size_t N)
+__global__ void hash_kernel(const char* __restrict__ d_in, uint64_t* __restrict__ d_out, size_t blk_sz, size_t N)
 {
-    size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N)
-        d_out[i] = MurmurHash64A_GPU(d_in + i*blk_sz, blk_sz);
+    size_t idx     = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t stride  = gridDim.x  * blockDim.x;
+    while (idx < N) {
+        d_out[idx] = MurmurHash64A_GPU(d_in + idx * blk_sz, blk_sz);
+        idx += stride;
+    }
 }
